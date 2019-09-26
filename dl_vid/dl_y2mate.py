b50b4e3bd02b2a09b9ebfb641ea7f02d1b380731
= <=>
2019-09-26 21:16:19 +0200

    quick bkp

--- dl_vid/dl_y2mate.py
+++ dl_vid/dl_y2mate.py
@@ -1,11 +1,14 @@
 from my_py import disp
 from static import constants as cst
 
+import time
+import itertools
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from bs4 import BeautifulSoup as bs
 
 
 def downloadVideosFromLinks(vids_urls):
@@ -16,13 +19,27 @@ def downloadVideosFromLinks(vids_urls):
         ### transform for y2mate
         delimiter_index = vid_url.find("=")
         full_url = cst.download_site + "/youtube/" + vid_url[delimiter_index+1:]
-
-        print(full_url)
-        print(disp.line)
+        # print(full_url)
+        # print(disp.line)
 
         ### get page for video download
         driver = webdriver.Firefox()
         driver.get(full_url)
+
+
+        for turn in itertools.count():
+            all_html = bs(driver.page_source, "html.parser")
+            tab = all_html.find_all('table', attrs={'class':'table table-bordered'})
+            # print(len(tab), "tables")
+            try:
+                table = tab[0] ### loading is complete
+                # print("turn:", turn, " | ", "table:", table)
+                ### once it 
+                # driver.quit()
+                # break
+            except:
+                # tab = all_html.find('table', attrs={'class':'table table-bordered'})
+                time.sleep(0.00001)
         
         print("download finished successfully ;-)")
         print(disp.line)
