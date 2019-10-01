3f4825a25c94ea9ae2512c92db6edab74f4cd687
= <=>
2019-10-02 01:47:05 +0200

    lil save -- still not working but very close

--- yt_dl/eytd.py
+++ yt_dl/eytd.py
@@ -29,34 +29,42 @@ def downloadVideosFromLinks(vids_urls):
             ### wait for the indicator to appear
             try:
                 eytd_button = all_html.find('button', attrs={'id':'eytd_btn'})
-                print("der button ist:", eytd_button)
+                # print("der button ist:", eytd_button)
                 # eytd_button.click()
                 ### select one format : try 720p —> 360p —> take 1st available
-                dl_links = all_html.find_all('div', attrs={'class':'eytd_list_item'}) ### includes header
+                # dl_links = all_html.find_all('div', attrs={'class':'eytd_list_item'}) 
+                dl_links = all_html.find_all('a', attrs={'target':"_blank"}) 
+                # print("the dl links:", len(dl_links))
                 row_index = getRowForQueriedBitrate(dl_links, '720p')
                 if row_index is None: 
                     row_index = getRowForQueriedBitrate(dl_links, '360p')
                     if row_index is None:
                         row_index = 1 ### last resort : take the first one available ( [0] is headers)
-                # dl_button = 
+                # print("row at index {} contains 720p".format(row_index))
+                dl_link = dl_links[row_index]
+                print("dl link will be:", dl_link)
+                # dl_link.click()
+                dl_url = dl_link['href']
+                # print("dl url will be:", dl_url)
+                browser.get(dl_url)
             except: pass
-        # print("video {} / {} — [ {} ] is being downloaded ... Please be patient ...".format(video_countesr, len(vids_urls), video_filename))
+            print(disp.line)
+        print("video {} / {} is being downloaded ... Please be patient ...".format(video_counter, len(vids_urls)))
         ### une fois le DL lancé, faire une boucle dans le vide tant que video_filename + ".part" est présent dans ~/Downloads/
         videos_downloading = dl_st.countUnfinishedDownloads(cst.path_downloads)
         while videos_downloading >= cst.max_simultaneous_downloads: ### little slowdowner to limit nb of simlt dls
             videos_downloading = dl_st.countUnfinishedDownloads(cst.path_downloads)
         # print("video [ {} ] finished downloading successfully.".format(video_filename))
-        # print("{} / {} videos done !".format(video_counter, len(vids_urls)))
-        print(disp.line)
+        print("{} / {} videos done !".format(video_counter, len(vids_urls)))
+        print(disp.star)
     return video_counter
 
 
 def getRowForQueriedBitrate(rows, q_bitrate):
     row_index = 0
     for row in rows:
-        row_index += 1
-        print(row)
-        print(disp.line)
+        # print(row)
+        # print(disp.line)
         ### [0] is headers (th)
         ### [other] : any can be 720p !
         bitrate = row.get_text()
@@ -65,3 +73,5 @@ def getRowForQueriedBitrate(rows, q_bitrate):
             # print(bitrate)
             # print(disp.line)
             return row_index
+        ### increment AFTER the return
+        row_index += 1        
