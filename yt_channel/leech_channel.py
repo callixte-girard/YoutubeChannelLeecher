8c8c837fdd9e9448b56b95bd91b393b2a223b6a1
= <=>
2019-10-08 18:04:36 +0200

    video is only downloaded if not marked as downloaded in Notion

--- yt_channel/leech_channel.py
+++ yt_channel/leech_channel.py
@@ -12,6 +12,7 @@ from notion.block import DividerBlock
 
 
 def leechChannelFromUrl(channel_url, cv_url): ### must add manually a channel collection in Notion first.
+    collection = coll.getCollectionFromViewUrl(cv_url)
 
     ## - get all videos links
     vids_urls = all_v.getVideosLinksFromChannelUrl(channel_url)
@@ -21,13 +22,15 @@ def leechChannelFromUrl(channel_url, cv_url): ### must add manually a channel co
     plsts_urls = plst.getPlaylistsLinksFromChannelUrl(channel_url)
     print("total playlists published by user [ {} ] : {}".format(channel_url, len(plsts_urls)), end=cst.star)
 
-    ## - for each video :
+    ## - scrape all videos THEN download them
+    vid_counter = 0
     for vid_url in vids_urls:
+        vid_counter += 1
+        print("progress (scraping infos) : {} / {}".format(vid_counter, len(vids_urls)))
         ## 1) check if it is already present in channel's Notion collection (from URL). If not...
-        collection = coll.getCollectionFromViewUrl(cv_url)
         vid = coll.getCorrespondingRowFromVidUrl(collection, vid_url)
         if vid is None:
-            print("video at {} doesn't exist. Let's scrape its infos.".format(vid_url))
+            print("video at [ {} ] doesn't exist. Let's scrape its infos.".format(vid_url))
             ## 2) ... create a Video with its url and the playlists it belongs. 
             vid = inf.scrapeVideoInfosFromLink(vid_url)
             # print("title: {} | published_on: {} —> scraped infos done :)".format(vid.title, vid.published_on), end=cst.line)
@@ -41,15 +44,15 @@ def leechChannelFromUrl(channel_url, cv_url): ### must add manually a channel co
             row.children.add_new(HeaderBlock, title="Decription de la vidéo")
             row.children.add_new(DividerBlock)
             row.children.add_new(TextBlock, title=vid.description)
-            print("video at {} successfully scraped infos and inserted into Notion :)".format(vid_url), end=cst.line)
+            print("video at [ {} ] successfully scraped infos and inserted into Notion :)".format(vid_url), end=cst.line)
         else:
-            print("video at {} already exists. Here is its title : [ {} ]".format(vid_url, vid.title), end=cst.line)
+            print("video at [ {} ] already exists in Notion yet. Here is its title : [ {} ]".format(vid_url, vid.title), end=cst.line)
             ## 3) check if its labels are the same as the playlists it belongs. (postponed)
 
 
     print("let's download all these cool vids now !", end=cst.star)
     ## - for each video, download it if "downloaded" is unchecked on Notion. Else, skip it.
-    downloaded_videos = dl_pytube.downloadVideosFromLinks(vids_urls)
+    downloaded_videos = dl_pytube.downloadVideosFromLinks(vids_urls, collection)
     ## - check "downloaded" on notion if successful. Else, leave blank.
     
 
--- yt_dl/dl_pytube.py
+++ yt_dl/dl_pytube.py
@@ -2,21 +2,35 @@ from static import constants as cst
 from static import variables as var
 from pytube import YouTube
 from pytube import exceptions as py_ex
+from notion_so import collection as coll
 
 
-def downloadVideosFromLinks(vids_urls):
-    video_counter = 0
+def downloadVideosFromLinks(vids_urls, collection):
+    vid_counter = 0
     for vid_url in vids_urls:
         # print(vid_url)
-        video_counter += 1      
+        vid_counter += 1      
         full_url = cst.url_main + vid_url
-        print("download video at :", full_url)
-        vid = YouTube(full_url).streams.filter(mime_type='video/mp4', res='720p').first()
-        print(vid)
-        try:
-            vid.download(cst.path_downloads)
-            print("video {} / {} has finished downloading ! :-)".format(video_counter, len(vids_urls)), end=cst.line)
-        except py_ex.VideoUnavailable as e:
-            print("video {} / {} could not be downloaded :-( here's why :".format(video_counter, len(vids_urls)), end=cst.line)
-            print(str(e))
-    return video_counter
+        print("progress (downloading) : {} / {}".format(vid_counter, len(vids_urls)))
+        ### check on Notion if video has already been downloaded or not
+        row = coll.getCorrespondingRowFromVidUrl(collection, vid_url)
+        if not row.downloaded:
+            print("video at [ {} ] will be downloaded ...".format(vid_url))
+            ### get appropriate bitrate and format
+            try:
+                vid = YouTube(full_url).streams.filter(mime_type='video/mp4', res='720p').first()
+                print(vid)
+                ### try to download video, if available
+                try:
+                    vid.download(cst.path_downloads)
+                    ### mark video as downloaded in Notion
+                    row.downloaded = True
+                    print("video has finished downloading ! :)", end=cst.line)
+                except py_ex.VideoUnavailable as e:
+                    print("video could not be downloaded :( here's why :", end=cst.line)
+                    print(str(e))
+            except:
+                print("wanted bitrate does not exist...")
+        else:
+            print("video at [ {} ] — [ {} ] has already been downloaded.".format(vid_url, row.title), end=cst.line)
+    return vid_counter
