8813e86bb84b0415755ee50a2d642eee4566bbb3
= <=>
2019-11-02 14:54:26 +0100

    readme updated, chrome setup correctly

--- static/methods.py
+++ static/methods.py
@@ -21,13 +21,9 @@ def addsParamsToUrl(url, params_names, params_values):
             if i < len(params_names)-1: url += "&"
     return url
 
+
 ### to init browser with proper settings
 def initChromiumConfiguredProperly():
-    ### tells to download in a custom path and sets it
-    # profile = webdriver.ChromeOptions()
-    # profile.set_preference("browser.download.folderList", 0)
-    # profile.set_preference("browser.download.manager.showWhenStarting", False)
-    # profile.set_preference("browser.download.dir", cst.path_downloads + "pipou")
     browser = webdriver.Chrome() 
     ### set half-part size for window
     browser.maximize_window()
@@ -35,18 +31,6 @@ def initChromiumConfiguredProperly():
     browser.set_window_position(0, 0)
     return browser
 
-def initFirefoxConfiguredProperly():
-    ### tells to download in a custom path and sets it
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("browser.download.folderList", 0)
-    profile.set_preference("browser.download.manager.showWhenStarting", False)
-    profile.set_preference("browser.download.dir", cst.path_downloads + "pipou")
-    browser = webdriver.Firefox(firefox_profile=profile) 
-    ### set half-part size for window
-    browser.maximize_window()
-    browser.set_window_size(browser.get_window_size()["width"] / 2, browser.get_window_size()["height"])
-    browser.set_window_position(0, 0)
-    return browser
 
 ### to count downloads
 def countUnfinishedDownloads(path):
