5a1b7ce9b8b8aa146e2f49d644769e2db83385d8
Callixte Girard <callixtegirard@gmail.com>
2020-12-05 10:17:29 +0100

    yéééé added file referencing urls to videos from notion

--- clean_csv_from_notion.py
+++ clean_csv_from_notion.py
@@ -3,6 +3,7 @@
 from pprint import pp
 import csv
 import sys
+import os
 
 
 csv_path = None
@@ -11,23 +12,28 @@ else: sys.exit(">>> You must specify one argument : the CSV complete file path. 
 # csv_path = "/Users/c/Downloads/All videos in channel 0a4ac35aa7894489b85af84ffacfefe9.csv"
 
 
-urls_to_write = []
-with open(csv_path) as csv_file:
+rows_to_write = []
+with open(csv_path, "r") as csv_file:
 	csv_content = csv.reader(csv_file)
 	for line_csv in csv_content:
 		# print(line_csv)
+		title = line_csv[0]
 		uri = [f for f in line_csv if "/watch?" in f]
 		if (len(uri)) > 0:
+			# extract value from the array
 			uri = uri[0]
 			# print('uri:', uri)
 			url = "https://youtube.com" + uri
 			# print('url:', url)
-			urls_to_write.append(url)
-# pp(urls_to_write)
+			rows_to_write.append([url, title])
+# pp(rows_to_write)
 
-
+# now create the path for the clean csv...
 cleaned_csv_path = csv_path.replace(".csv", ".cleaned") + ".csv"
-with open(cleaned_csv_path, "w+", newline="") as csv_file:
+# ...and "pre-overwrite" it if it already exists
+if os.path.isfile(cleaned_csv_path): os.remove(cleaned_csv_path)
+
+with open(cleaned_csv_path, "w", newline="") as csv_file:
 	writer = csv.writer(csv_file)
-	writer.writerows(urls_to_write)
-	print("writing cleaned urls done!")
+	writer.writerows(rows_to_write)
+	print(">>> Writing cleaned urls done!")
